afb1a48227e96069df8bd3e60ea81f75e56fe5d9
NamOSeoung <developernam92@gmail.com>
2020-12-24 14:45:17 +0900

    신규 (프로젝트)
    * init

---
+++ Pinpli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pinpli
+//
+//  Created by 남오승 on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
