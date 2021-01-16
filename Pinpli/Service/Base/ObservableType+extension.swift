31e42a51f288c946411e7b429c9c58a4e1da448d
남오승 <developernam92@gmail.com>
2021-01-16 23:35:08 +0900

    수정 * 안쓰는 소스 제거

--- Pinpli/Service/Base/ObservableType+extension.swift
+++ Pinpli/Service/Base/ObservableType+extension.swift
@@ -27,23 +27,4 @@ extension ObservableType {
             return Observable.just(object)
         }
     }
-    
-    public func mapArray<T: Codable>(type: T.Type) -> Observable<[T]> {
-        return flatMap { data -> Observable<[T]> in
-            let responseTuple = data as? (HTTPURLResponse, Data)
-            
-            guard let jsonData = responseTuple?.1 else {
-                throw NSError(
-                    domain: "",
-                    code: -1,
-                    userInfo: [NSLocalizedDescriptionKey: "JSON모델 디코딩 에러"]
-                )
-            }
-            
-            let decoder = JSONDecoder()
-            let objects = try decoder.decode([T].self, from: jsonData)
-            
-            return Observable.just(objects)
-        }
-    }
 }
