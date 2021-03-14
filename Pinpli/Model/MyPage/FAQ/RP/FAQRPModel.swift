dd223715b13fbae75b145c4fc39ea0b663c903df
남오승 <developernam92@gmail.com>
2021-03-14 23:32:02 +0900

    신규(RPModel) * 자주하는 질문조회 ResponseModel 추가

---
+++ Pinpli/Model/MyPage/FAQ/RP/FAQRPModel.swift
@@ -0,0 +1,42 @@
+//
+//  FAQRPModel.swift
+//  Pinpli
+//
+//  Created by 남오승 on 2021/03/14.
+//
+
+import Foundation
+
+struct FAQRPModel : Codable {
+    
+    let data : FAQRPModelData?
+    
+    enum CodingKeys: String, CodingKey {
+        case data = "data"
+    }
+}
+
+struct FAQRPModelData : Codable {
+    
+    let faqs : [Faq]?
+    
+    enum CodingKeys: String, CodingKey {
+        case faqs = "faqs"
+    }
+}
+
+struct Faq : Codable {
+    
+    let content : String?
+    let createdAt : String?
+    let faqId : String?
+    let subject : String?
+    
+    enum CodingKeys: String, CodingKey {
+        case content = "content"
+        case createdAt = "created_at"
+        case faqId = "faq_id"
+        case subject = "subject"
+    }
+    
+}
