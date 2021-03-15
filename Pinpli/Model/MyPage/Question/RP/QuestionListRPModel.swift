62908c3d2e832f82642d609c2c5c5f2569762b72
남오승 <developernam92@gmail.com>
2021-03-15 19:04:58 +0900

    신규(RPModel) * 1:1문의리스트 전용 ResponseModel 추가

---
+++ Pinpli/Model/MyPage/Question/RP/QuestionListRPModel.swift
@@ -0,0 +1,32 @@
+//
+//  QuestionListRPModel.swift
+//  Pinpli
+//
+//  Created by 남오승 on 2021/03/15.
+//
+
+import Foundation
+
+//1:1문의 리스트 전용 Response Model
+struct QuestionListRPModel : Codable {
+    let errors: ErrorRPModel?
+    let data : QuestionListRPModelData?
+}
+
+struct QuestionListRPModelData : Codable {
+    let questions : [QuestionModel]?
+}
+
+struct QuestionModel : Codable {
+    let content : String?
+    let createdAt : String?
+    let questionId : String?
+    let status : String?
+    
+    enum CodingKeys: String, CodingKey {
+        case content = "content"
+        case createdAt = "created_at"
+        case questionId = "question_id"
+        case status = "status"
+    }
+}
