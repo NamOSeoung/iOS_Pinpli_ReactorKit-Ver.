2da781b6eb11882314782b5646ed2111668b551c
남오승 <developernam92@gmail.com>
2021-05-08 18:28:11 +0900

    신규(Class) * 상점보기 탭전용 제스쳐 클래스 추가

---
+++ Pinpli/ViewUI/TapGesture/StoreTapGesture.swift
@@ -0,0 +1,13 @@
+//
+//  StoreTapGesture.swift
+//  Pinpli
+//
+//  Created by 남오승 on 2021/03/03.
+//
+
+import UIKit
+
+class StoreVCTapGesture: UITapGestureRecognizer {
+    //리뷰 아이디
+    var reviewId:String = String()
+}
